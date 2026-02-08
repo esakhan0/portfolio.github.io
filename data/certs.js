1f75aaaac562ecc6a09c506600410110d0e85d1f
Mr Esa Khan <channelkhan0@gmail.com>
2026-02-08 20:56:26 +0000

    push

--- data/certs.js
+++ data/certs.js
@@ -2,16 +2,33 @@
 window.CERT_TAGS = [
   // { key: "all", label: "All" },              // keep "all"
   { key: "school", label: "School" },
-  { key: "coden", label: "Code Ninjas" }
+  { key: "coden", label: "Code Ninjas" },
+  { key: "flying", label: "Flight Cert" },
+  { key: "thai", label: "Thaitanz Muai Thai" },
+  { key: "swim", label: "Swimming" }
 ];
 
 // Certificates (edit this list only)
 window.CERTS = [
   {
-    title: "Maths Achievement Award",
+    title: "End Of Year Award - Academic - Art - 9B",
     tag: "school",
     issuer: "My School",
-    date: "2025-12-10",
-    image: "/images/panel.png"
+    date: "2025-07-10",
+    image: "/certs/All Certificates.jpg"
+  },
+  {
+    title: "Certificate of Acheivement - English - 1A",
+    tag: "school",
+    issuer: "My School",
+    date: "2017-06-14",
+    image: "/certs/All Certificates_1.jpg"
+  },
+  {
+    title: "End of Year Award - Effort - 6A ",
+    tag: "school",
+    issuer: "My School",
+    date: "2022-02-10",
+    image: "/certs/All Certificates_2.jpg"
   }
 ];
