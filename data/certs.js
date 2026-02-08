f433137d9a5119d9abb0d74982f327c5abea0452
Mr Esa Khan <channelkhan0@gmail.com>
2026-02-08 12:32:23 +0000

    push

---
+++ data/certs.js
@@ -0,0 +1,34 @@
+// Tags/categories you can add/remove anytime
+window.CERT_TAGS = [
+  { key: "all", label: "All" },              // keep "all"
+  { key: "school", label: "School" },
+  { key: "coding", label: "Coding" },
+  { key: "networking", label: "Networking" },
+  { key: "other", label: "Other" },
+  { key: "test", label: "Test" }
+];
+
+// Certificates (edit this list only)
+window.CERTS = [
+  {
+    title: "Maths Achievement Award",
+    tag: "school",
+    issuer: "My School",
+    date: "2025-12-10",
+    image: "/images/certs/maths-award.jpg"
+  },
+  {
+    title: "Python Beginner Certificate",
+    tag: "coding",
+    issuer: "SoloLearn",
+    date: "2026-02-03",
+    image: "/images/certs/python-beginner.jpg"
+  },
+  {
+    title: "Networking Fundamentals",
+    tag: "networking",
+    issuer: "Cisco",
+    date: "2025-11-20",
+    image: "/images/certs/networking-fundamentals.jpg"
+  }
+];
