c2a1975086109cf86f1b23dbd2516556a5c22741
Mr Esa Khan <channelkhan0@gmail.com>
2026-02-10 17:38:30 +0000

    push

--- data/certs.js
+++ data/certs.js
@@ -1,34 +1,194 @@
-// Tags/categories you can add/remove anytime
 window.CERT_TAGS = [
-  // { key: "all", label: "All" },              // keep "all"
-  { key: "school", label: "School" },
-  { key: "coden", label: "Code Ninjas" },
-  { key: "flying", label: "Flight Cert" },
-  { key: "thai", label: "Thaitanz Muai Thai" },
-  { key: "swim", label: "Swimming" }
+  { key: "School", label: "School" },
+  { key: "Code Ninjas", label: "Code Ninjas" },
+  { key: "Flying Certificate", label: "Flying Certificate" },
+  { key: "Thaitanz", label: "Thaitanz" },
+  { key: "Swimming", label: "Swimming" },
+  { key: "Hifz", label: "Quran" }
 ];
 
-// Certificates (edit this list only)
+
 window.CERTS = [
   {
     title: "End Of Year Award - Academic - Art - 9B",
-    tag: "school",
-    issuer: "My School",
+    tag: "School",
+    issuer: "Al Khair Secondary",
     date: "2025-07-10",
-    image: "/certs/All Certificates.jpg"
+    image: "images/certs/All Certificates.jpg"
   },
   {
-    title: "Certificate of Acheivement - English - 1A",
-    tag: "school",
-    issuer: "My School",
+    title: "Certificate of Achievement - English - 1A",
+    tag: "School",
+    issuer: "Al Khair Primary",
     date: "2017-06-14",
-    image: "/certs/All Certificates_1.jpg"
+    image: "images/certs/All Certificates_1.jpg"
   },
   {
-    title: "End of Year Award - Effort - 6A ",
-    tag: "school",
-    issuer: "My School",
+    title: "End of Term Award - Effort - 6A ",
+    tag: "School",
+    issuer: "Al Khair Primary",
     date: "2022-02-10",
-    image: "/certs/All Certificates_2.jpg"
-  }
+    image: "images/certs/All Certificates_2.jpg"
+  },
+  {
+    title: "Flight Certificate - PA28 - REHILL AVIATION",
+    tag: "Flying Certificate",
+    issuer: "Redhill Aviation",
+    date: "2023-08-02",
+    image: "images/certs/All Certificates_4.jpg"
+  },
+  {
+    title: "End Of Year Award - Maths - 6A",
+    tag: "School",
+    issuer: "Al Khair Primary",
+    date: "2022-06-25",
+    image: "images/certs/All Certificates_5.jpg"
+  },
+  {
+    title: "End Of Year Award - Effort - 5B",
+    tag: "School",
+    issuer: "Al Khair Primary",
+    date: "2021-03-12",
+    image: "images/certs/All Certificates_7.jpg"
+  },
+  {
+    title: "The Best Dressed - World Book Day - 9B",
+    tag: "School",
+    issuer: "Al Khair Secondary",
+    date: "2024-12-03",
+    image: "images/certs/All Certificates_8.jpg"
+  },
+  {
+    title: "Rainbow Distance - 200M",
+    tag: "Swimming",
+    issuer: "Swim England",
+    date: "",
+    image: "images/certs/All Certificates_9.jpg"
+  },
+  {
+    title: "Learn to swim - Class 5",
+    tag: "Swimming",
+    issuer: "Swim England",
+    date: "",
+    image: "images/certs/All Certificates_6.jpg"
+  },
+  {
+    title: "Completion of Yellow Belt",
+    tag: "Code Ninjas",
+    issuer: "Code Ninjas",
+    date: "2022-12-06",
+    image: "images/certs/All Certificates_12.jpg"
+  },
+  {
+    title: "Completion of White Belt",
+    tag: "Code Ninjas",
+    issuer: "Code Ninjas",
+    date: "2021-12-12",
+    image: "images/certs/All Certificates_14.jpg"
+  },
+  {
+    title: "Completion of Orange Belt",
+    tag: "Code Ninjas",
+    issuer: "Code Ninjas",
+    date: "2022-06-25",
+    image: "images/certs/All Certificates_16.jpg"
+  },
+  {
+    title: "Rainbow Distance - 1000M",
+    tag: "Swimming",
+    issuer: "Swim England",
+    date: "",
+    image: "images/certs/All Certificates_17.jpg"
+  },
+  {
+    title: "Learn to swim - Class 6",
+    tag: "Swimming",
+    issuer: "Swim England",
+    date: "",
+    image: "images/certs/All Certificates_18.jpg"
+  },
+  {
+    title: "Completion of HACKERS WANTED: UNCOVER THE CODE IN ROBLOX",
+    tag: "Code Ninjas",
+    issuer: "Code Ninjas",
+    date: "",
+    image: "images/certs/All Certificates_19.jpg"
+  },
+  {
+    title: "Completion of POTENTIAL UNLOCKED: BECOME A ROBLOX DEVELOPER",
+    tag: "Code Ninjas",
+    issuer: "Code Ninjas",
+    date: "",
+    image: "images/certs/All Certificates_20.jpg"
+  },
+  {
+    title: "Spring Award - CS - 8B",
+    tag: "School",
+    issuer: "Al Khair Secondary",
+    date: "2024-03-22",
+    image: "images/certs/All Certificates_21.jpg"
+  },
+  {
+    title: "End Of Term Award - Acheivement - Arabic - 3B",
+    tag: "School",
+    issuer: "Al Khair Secondary",
+    date: "2019-05-24",
+    image: "images/certs/All Certificates_22.jpg"
+  },
+  {
+    title: "Learn to swim - Class 4",
+    tag: "Swimming",
+    issuer: "Swim England",
+    date: "",
+    image: "images/certs/All Certificates_23.jpg"
+  },
+  {
+    title: "Star Of The Month",
+    tag: "Thaitanz",
+    issuer: "Thaitanz Coaches",
+    date: "2024-04-23",
+    image: "images/certs/All Certificates_24.jpg"
+  },
+  {
+    title: "Learn to swim - Class 2",
+    tag: "Swimming",
+    issuer: "Swim England",
+    date: "",
+    image: "images/certs/All Certificates_3.jpg"
+  },
+  {
+    title: "End Of Year Award - Physics - 9B",
+    tag: "School",
+    issuer: "Al Khair Primary",
+    date: "2025-06-12",
+    image: "images/certs/All Certificates_10.jpg"
+  },
+  {
+    title: "Summer Hifdh Intensive 2024",
+    tag: "Hifz",
+    issuer: "MCWAS",
+    date: "2024-08-24",
+    image: "images/certs/All Certificates_11.jpg"
+  },
+  {
+    title: "Student Of The Week - 8B",
+    tag: "School",
+    issuer: "Al Khair Secondary",
+    date: "2024-01-26",
+    image: "images/certs/All Certificates_13.jpg"
+  },
+  {
+    title: "Acheivement Award",
+    tag: "Swimming",
+    issuer: "Swim England",
+    date: "",
+    image: "images/certs/All Certificates_15.jpg"
+  },
+  {
+    title: "Sky Up - Trip Lanyard",
+    tag: "School",
+    issuer: "Sky Studios",
+    date: "2026-02-10",
+    image: "images/certs/sky.jpg"
+  },
 ];
